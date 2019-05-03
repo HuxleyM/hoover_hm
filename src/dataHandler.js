f3977064539dd1b2bf4bf00754d2b532d75cf611
Huxley_Millard <gerogiamillardccm@gmail.com>
2019-05-03 13:19:34 +0100

    gonna swap back to a functional approach

--- src/dataHandler.js
+++ src/dataHandler.js
@@ -1,35 +1,37 @@
 const fs = require('fs');
 
 
-const DataHandler = (function(){
+function readData(){
+    return fs.readFileSync('index.txt', 'utf8').split('\n');
+}
 
-    function readData(){
-        return fs.readFileSync('index.txt', 'utf8').split('\n');
-    }
+function turnToNumbersArray(data){
+    return  data[0].split(' ').map(x => parseInt(x))
+}
 
-    function turnToNumbersArray(data){
-        return  data[0].split(' ').map(x => parseInt(x))
-    }
+function sortData(data){
+    var results = {}
+    results.grid = turnToNumbersArray(data.splice(0,1))
+    results.location = turnToNumbersArray(data.splice(0,1))
+    results.directions = data.splice(data.length-1, 1)[0]
+    results.dirtPatches = data.map(x => {
+      x = x.split(' ')
+      return [parseInt(x[0]),parseInt(x[1])]
+    })
+    return results
+}
 
-    function sortData(data){
-        var results = {}
-        results.grid = turnToNumbersArray(data.splice(0,1))
-        results.location = turnToNumbersArray(data.splice(0,1))
-        results.directions = data.splice(data.length-1, 1)[0]
-        results.dirtPatches = data.map(x => {
-          x = x.split(' ')
-          return [parseInt(x[0]),parseInt(x[1])]
-        })
-        return results
-    }
+
+const DataHandler = (function(fs){
+
 
     return {
-        readData : readData,
+        
         sortData : sortData,
         turnToNumbersArray: turnToNumbersArray
     }
 
-})();
+})(fs);
 
 
 module.exports = DataHandler
